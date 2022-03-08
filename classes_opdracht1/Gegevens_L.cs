bc0e3530eef4969a423f3abd14f5864530884d7c
Thibe Provost <thibe.provost@viso-cor-mariae.be>
2022-03-08 09:51:22 +0100

    Add project files.

---
+++ classes_opdracht1/Gegevens_L.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classes_opdracht1
+{
+    class Gegevens_L
+    {
+        public string strVoornaam;
+        public string strAchternaam;
+        public string strKlas;
+        public Gegevens_L()
+        {
+            Console.Write("Geef uw voornaam: ");
+            strVoornaam = Console.ReadLine();
+            Console.Write("Geef uw achternaam: ");
+            strAchternaam = Console.ReadLine();
+            Console.Write("Geef uw klas: ");
+            strKlas = Console.ReadLine();
+            Console.Clear();
+        }
+        public void Aanpassen(Gegevens_L leerling)
+        {
+            Console.Write("Geef uw voornaam: ");
+            strVoornaam = Console.ReadLine();
+            Console.Write("Geef uw achternaam: ");
+            strAchternaam = Console.ReadLine();
+            Console.Write("Geef uw klas: ");
+            strKlas = Console.ReadLine();
+            Console.Clear();
+        }
+
+        public void Weergeven(Gegevens_L leerling)
+        {
+            Console.WriteLine($"Voornaam leerling: {strVoornaam}");
+            Console.WriteLine($"Achternaam leerling: {strAchternaam}");
+            Console.WriteLine($"Klas leerling: {strKlas}");
+        }
+    }
+}
---
+++ classes_opdracht1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace classes_opdracht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gegevens_L Leerling = new Gegevens_L();
+            string strAnswer;
+            Console.Write("Wilt u iets aanpassen?(Y/N) ");
+            strAnswer = Console.ReadLine().ToUpper();
+            if (strAnswer == "Y")
+            {
+                Leerling.Aanpassen(Leerling);
+            }
+            Leerling.Weergeven(Leerling);
+        }
+    }
+}
